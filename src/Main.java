d84463040fb686f81215fbc94cc33577e5ffa5cf
nickfielderawesome <nickfielderawesome@gmail.com>
2017-11-18 14:41:49 -0500

    added jsoup functionality to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 /**
  * Created by Chris on 11/18/2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Document document = Jsoup.connect("http://google.com").get();
+            System.out.println(document.html());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
