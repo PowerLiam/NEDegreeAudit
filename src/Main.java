ce25e021fd7aeff1d973f169c07afcb0de05aec3
nickfielderawesome <nickfielderawesome@gmail.com>
2017-11-18 14:50:06 -0500

    added jsoup functionality to main

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
         try {
             Document document = Jsoup.connect("http://google.com").get();
             System.out.println(document.html());
-
         } catch (IOException e) {
             e.printStackTrace();
         }
